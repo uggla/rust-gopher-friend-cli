93df41b5dc102c0e624174fc8a64993293d9f744
Uggla <uggla@free.fr>
2021-08-01 17:56:51 +0200

    feat: Add completion command
    - Add completion command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
 use std::io::stdout;
-use std::io::Write;
 use structopt::clap::{crate_name, crate_version, Shell};
 use structopt::StructOpt;
 
@@ -21,29 +19,10 @@ enum Command {
     },
 }
 
-fn get_gopher(gopher: String) {
-    println!("Try to get {} Gopher...", gopher);
-    let url = format!("https://github.com/scraly/gophers/raw/main/{}.png", gopher);
-    let response = minreq::get(url)
-        .send()
-        .expect("Fail to get response from server");
-
-    if response.status_code == 200 {
-        let file_name = format!("{}.png", gopher);
-        let mut output_file = File::create(&file_name).expect("Fail to create file");
-        output_file
-            .write_all(response.as_bytes())
-            .expect("Fail to write file");
-        println!("Perfect! Just saved in {}", &file_name);
-    } else {
-        eprintln!("Gopher {} not exists", gopher);
-    }
-}
-
 fn main() {
     let cmd = Command::from_args();
     match cmd {
-        Command::Get { gopher } => get_gopher(gopher),
+        Command::Get { gopher } => println!("{}", gopher),
         Command::Completion { shell } => {
             Command::clap().gen_completions_to(crate_name!(), shell, &mut stdout())
         }
